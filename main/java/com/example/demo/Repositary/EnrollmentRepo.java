a9a63ba1eb84d5a22458b8b701c9bbd77f65c228
Aya Mohamed <115155688+Aya921@users.noreply.github.com>
2024-12-27 20:24:32 +0200

    Update EnrollmentRepo.java

--- main/java/com/example/demo/Repositary/EnrollmentRepo.java
+++ main/java/com/example/demo/Repositary/EnrollmentRepo.java
@@ -1,5 +1,6 @@
 package com.example.demo.Repositary;
 
+
 import com.example.demo.Entity.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
