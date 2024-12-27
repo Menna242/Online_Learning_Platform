b708bd970654af5dfd83a4d33559514a5763af4a
MariamMahmoud2003 <146382004+MariamMahmoud2003@users.noreply.github.com>
2024-12-27 10:09:37 -0800

    Update AssigmentRepo.java

--- main/java/com/example/demo/Repositary/AssigmentRepo.java
+++ main/java/com/example/demo/Repositary/AssigmentRepo.java
@@ -1,9 +1,7 @@
 // mariam
 package com.example.demo.Repositary;
-
 import com.example.demo.Entity.Assigment;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface AssigmentRepo extends JpaRepository<Assigment, Long> {
