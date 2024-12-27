f018745db78110c95243f1b00a2ed50e51c85f02
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2024-12-27 20:27:15 +0200

    Update JWTAuthenticationFilter.java

--- main/java/com/example/demo/config/JWTAuthenticationFilter.java
+++ main/java/com/example/demo/config/JWTAuthenticationFilter.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+//Authentication 
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
